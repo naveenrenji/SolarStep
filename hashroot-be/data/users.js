20e194f07883459dc4eba30c9b278c1e626abd58
Naveen Mathews <52288469+naveenrenji@users.noreply.github.com>
2023-03-08 00:13:25 -0500

    redid Backend Code for Users
    Refactored Anmols backend code and added neccessary changes to folder structure.

--- hashroot-be/data/users.js
+++ hashroot-be/data/users.js
@@ -1,38 +1,45 @@
+import bcryptjs from "bcryptjs";
 import { ObjectId } from "mongodb";
-import { users } from "../config/mongoCollections.js";
-import validations from "./validation.js";
+import {
+  checkId,
+  checkRole,
+  checkLname,
+  checkFname,
+  checkEmail,
+  checkPassword,
+} from "../helpers.js";
 
-export const createUser = async (
-  firstName,
-  lastName,
-  password,
-  email,
-  role
-) => {
-  validations.checkString(firstName);
-  validations.checkString(lastName);
-  validations.checkString(password);
-  validations.checkString(role);
-  validations.checkString(email);
+const SALT_ROUNDS = 10;
 
+const isUserInDb = async (email) => {
+  const userCollection = await users();
+  const user = await userCollection.findOne({ email: email });
+  if (user === null) throw "Either the email or password is invalid";
+  return user;
+};
+
+const hashPassword = async (password) => {
+  const salt = await bcryptjs.genSalt(SALT_ROUNDS);
+  const hash = await bcryptjs.hash(password, salt);
+  return hash;
+};
+
+const createUser = async (firstName, lastName, password, email, roles) => {
+  checkFname(firstName);
+  checkLname(lastName);
+  checkPassword(password);
+  checkRole(roles);
+  checkEmail(email);
   firstName = firstName.trim();
   lastName = lastName.trim();
   email = email.trim();
-
-  const user = {
-    firstName,
-    lastName,
-    email,
-    password,
-    role,
-  };
-
-  const usercollection = await users();
-  const result = await usercollection.insertOne(user);
+  const hashedPassword = await hashPassword(password);
+  const user = { firstName, lastName, email, password: hashedPassword, roles };
+  const userCollection = await users();
+  const result = await userCollection.insertOne(user);
   const insertedId = result.insertedId;
-
-  const insertedUser = await usercollection.findOne({ _id: insertedId });
-  const end_user = {
+  const insertedUser = await userCollection.findOne({ _id: insertedId });
+  const endUser = {
     _id: insertedUser._id.toString(),
     firstName: insertedUser.firstName,
     lastName: insertedUser.lastName,
@@ -40,34 +47,66 @@ export const createUser = async (
     roles: insertedUser.roles,
     email: insertedUser.email,
   };
-  return end_user;
+  return endUser;
 };
 
-export const get = async (id) => {
-  validations.checkId(id);
-  const usercollection = await users();
-  const single_user = await usercollection.findOne({ _id: new ObjectId(id) });
-  if (!single_user) {
-    throw "No user is found with that id.";
-  }
-  single_user._id = single_user._id.toString();
-  return single_user;
-};
-
-export const getAll = async () => {
-  const usercollection = await users();
-  const userList = await usercollection.find({}).toArray();
+const getAllUsers = async () => {
+  const userCollection = await users();
+  const userList = await userCollection.find({}).toArray();
   if (userList.length === 0) {
-    throw Error("Unable to retrieve all users.");
+    throw new Error("Unable to retrieve all users.");
   }
-
-  let finalUserList = userList.map((user) => ({
+  const finalUserList = userList.map((user) => ({
     _id: user._id.toString(),
     firstName: user.firstName,
     lastName: user.lastName,
     email: user.email,
     password: user.password,
-    role: user.role,
+    roles: user.roles,
   }));
   return finalUserList;
 };
+
+const getUserById = async (id) => {
+  checkId(id);
+  const userCollection = await users();
+  const user = await userCollection.findOne({ _id: new ObjectId(id) });
+
+  if (!user) {
+    throw new Error("No user found with that id.");
+  }
+  user._id = user._id.toString();
+  return user;
+};
+
+const deleteUserById = async (id) => {
+  checkId(id);
+  const userCollection = await users();
+  const user = await userCollection.findOneAndDelete({ _id: new ObjectId(id) });
+
+  if (!user.value) {
+    throw new Error(`Could not delete user with id of ${id}.`);
+  }
+
+  return `${user.value.firstName} has been successfully deleted!`;
+};
+
+const loginUser = async (email, password) => {
+  email = checkEmail(email);
+  password = checkPassword(password);
+  const user = isUserInDb(email);
+  let realPassword = user.password;
+  let compareToMatch = await bcryptjs.compare(password, realPassword);
+  if (compareToMatch) {
+    return {
+      _id: user._id,
+      email: user.email,
+      Fname: user.Fname,
+      Lname: user.Lname,
+    };
+  } else {
+    throw "Either the email or password is invalid";
+  }
+};
+
+export { deleteUserById, getUserById, getAllUsers, createUser, loginUser };
