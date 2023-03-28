7a016b41d3f4e64fd65a4b34af76273b78894695
RealOkabe <anmolzagrawal@gmail.com>
2023-03-28 13:52:08 -0400

    Recreated form to follow the project's theme

--- hashroot/src/components/Profile/index.js
+++ hashroot/src/components/Profile/index.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import { toast } from "react-toastify";
 import RouteHeader from "../shared/RouteHeader";
 import { updateProfile } from "../../api/users";
 import useAuth from "../../hooks/useAuth"
+import SubmitButton from "../shared/SubmitButton";
 
 const Profile = () => {
   const auth = useAuth()
@@ -10,6 +14,7 @@ const Profile = () => {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setFirstName(auth?.user?.firstName)
@@ -37,43 +42,101 @@ const Profile = () => {
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const body = {firstName, lastName, oldPassword, newPassword}
-    const user = await updateProfile(body);
-    auth.updateProfile(user)
-    // handle form submission here
+    try {
+      setLoading(true);
+      e.preventDefault();
+      const body = {firstName, lastName, oldPassword, newPassword}
+      const user = await updateProfile(body);
+      auth.updateProfile(user)
+      // Handle form submission here.
+    } catch (e) {
+      toast(e?.response?.data?.error || e?.message || "Something went wrong", {
+        type: toast.TYPE.ERROR,
+      });
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return (
-    <div>
-      <RouteHeader headerText="Edit Profile" />
-      <br/>
-      <br/>
-      <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
-        <label style={{marginBottom: "1rem"}}>
-          First Name:
-          <input type="text" value={firstName} onChange={handleFirstNameChange} style={{marginLeft: "1rem"}} />
-        </label>
-        <label style={{marginBottom: "1rem"}}>
-          Last Name:
-          <input type="text" value={lastName} onChange={handleLastNameChange} style={{marginLeft: "1rem"}} />
-        </label>
-        <label style={{marginBottom: "1rem"}}>
-          Old Password:
-          <input type="password" value={oldPassword} onChange={handleOldPasswordChange} style={{marginLeft: "1rem"}} />
-        </label>
-        <label style={{marginBottom: "1rem"}}>
-          New Password:
-          <input type="password" value={newPassword} onChange={handleNewPasswordChange} style={{marginLeft: "1rem"}} />
-        </label>
-        <label style={{marginBottom: "1rem"}}>
-          Confirm New Password:
-          <input type="password" value={confirmNewPassword} onChange={handleConfirmNewPasswordChange} style={{marginLeft: "1rem"}} />
-        </label>
-        <button type="submit" style={{marginTop: "1rem"}}>Save Changes</button>
-      </form>
-    </div>
-  );
+return (
+  <div>
+    <Card style={{ width: "24rem" }} className="mx-auto shadow-sm">
+      <Card.Header as="h5" className="text-center">
+        Edit User Profile
+      </Card.Header>
+      <Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              value={firstName}
+              type="firstName"
+              onChange={handleFirstNameChange}
+              placeholder="First Name"
+            />
+            <Form.Control.Feedback type="invalid">
+              First Name is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              value={lastName}
+              type="lastName"
+              onChange={handleLastNameChange}
+              placeholder="Last Name"
+            />
+            <Form.Control.Feedback type="invalid">
+              Last Name is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Old Password</Form.Label>
+            <Form.Control
+              value={oldPassword}
+              type="pass"
+              onChange={handleOldPasswordChange}
+              placeholder="*********"
+            />
+            <Form.Control.Feedback type="invalid">
+              Password is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>New Password</Form.Label>
+            <Form.Control
+              value={newPassword}
+              type="pass"
+              onChange={handleNewPasswordChange}
+              placeholder="*********"
+            />
+            <Form.Control.Feedback type="invalid">
+              Password is required
+            </Form.Control.Feedback>
+            </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+            <Form.Label>Confirm New Password</Form.Label>
+            <Form.Control
+              value={confirmNewPassword}
+              type="pass"
+              onChange={handleConfirmNewPasswordChange}
+              placeholder="*********"
+            />
+            <Form.Control.Feedback type="invalid">
+              Password is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <SubmitButton type="submit" variant="primary" loading={loading}>
+              Save Changes
+            </SubmitButton>
+          </div>
+        </Form>
+      </Card.Body>
+    </Card>
+    
+  </div>
+);
 };
 
 export default Profile;
