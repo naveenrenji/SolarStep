23f10fe44ce6d7d6655df2421501f16530acfc21
rajguru <rajguru7337@gmail.com>
2023-04-07 20:14:23 -0400

    SS-22 & SS-23

--- hashroot-be/data/projectStatuses.js
+++ hashroot-be/data/projectStatuses.js
@@ -1,6 +1,8 @@
-import { projectStatusLogs } from "../config/mongoCollections.js";
+import { projectStatusLogs, projects } from "../config/mongoCollections.js";
 import { PROJECT_STATUSES } from "../constants.js";
 import { checkProjectStatus } from "../helpers.js";
+import { ObjectId } from "mongodb";
+import { getProjectById } from "./projects.js";
 
 const createProjectLog = async (
   currentUser,
@@ -43,8 +45,63 @@ const moveToOnSiteInspectionScheduled = async (
   project,
   onSiteInspectionDate
 ) => {
+  if (!currentUser) throw "User not logged in";
   const status = PROJECT_STATUSES.ON_SITE_INSPECTION_SCHEDULED;
   // Write the code here
+
+  let projectCollections = await projects();
+  const updatedProjectLog = projectCollections.findOneAndUpdate(
+    { _id: new ObjectId(project._id) },
+    {
+      $set: {
+        status: status,
+        onSiteInspectionDate: onSiteInspectionDate,
+      },
+    },
+    { returnDocument: "after" }
+  );
+  if (updatedProjectLog.lastErrorObject.n !== 1 || !updatedProjectLog.value) {
+    throw new Error("Status for On-Site Inspection could not be changed.");
+  }
+
+  const updatedProject = await getProjectById(
+    currentUser,
+    project._id.toString()
+  );
+  return updatedProject;
 };
 
-export { createProjectLog, moveToOnSiteInspectionScheduled };
+const moveToOnSiteInspectionInProgress = async (
+  currentUser,
+  project,
+  onSiteInspectionStartedOn
+) => {
+  if (!currentUser) throw "User not logged in";
+  const status = PROJECT_STATUSES.ON_SITE_INSPECTION_IN_PROGRESS;
+
+  let projectCollections = await projects();
+  const updatedProjectLog = projectCollections.findOneAndUpdate(
+    { _id: new ObjectId(project._id) },
+    {
+      $set: {
+        status: status,
+        onSiteInspectionStartedOn: onSiteInspectionStartedOn,
+      },
+    },
+    { returnDocument: "after" }
+  );
+  if (updatedProjectLog.lastErrorObject.n !== 1 || !updatedProjectLog.value) {
+    throw new Error("Status for On-Site Inspection could not be changed.");
+  }
+  const updatedProject = await getProjectById(
+    currentUser,
+    project._id.toString()
+  );
+  return updatedProject;
+};
+
+export {
+  createProjectLog,
+  moveToOnSiteInspectionScheduled,
+  moveToOnSiteInspectionInProgress,
+};
