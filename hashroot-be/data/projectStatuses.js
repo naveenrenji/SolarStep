97072032364188a08cb371c6c60f795853997e63
Atishay Jain <ajain70@stevens.edu>
2023-04-07 21:09:15 -0400

    Added data and routes for: 'Backend for an installation has started Backend'.

--- hashroot-be/data/projectStatuses.js
+++ hashroot-be/data/projectStatuses.js
@@ -1,6 +1,8 @@
+import { ObjectId } from "mongodb";
 import { projectStatusLogs } from "../config/mongoCollections.js";
 import { PROJECT_STATUSES } from "../constants.js";
 import { checkProjectStatus } from "../helpers.js";
+import { USER_ROLES } from "../constants.js";
 
 const createProjectLog = async (
   currentUser,
@@ -38,13 +40,78 @@ const createProjectLog = async (
   return true;
 };
 
-const moveToOnSiteInspectionScheduled = async (
-  currentUser,
-  project,
-  onSiteInspectionDate
-) => {
-  const status = PROJECT_STATUSES.ON_SITE_INSPECTION_SCHEDULED;
-  // Write the code here
+// completed status below
+const ProjectStatusLog = (currentUser, project, from) => {
+  if (currentUser.role !== USER_ROLES.ADMIN && currentUser.role !== USER_ROLES.SALES_REP) {
+    throw 'Only admins and sales reps can update project status to completed';
+}
+const _id = new ObjectId();
+const projectId = new ObjectId(project._id);
+const user = {
+  _id: new ObjectId(currentUser._id),
+  email: currentUser.email,
+  role: currentUser.role,
+};
+return {
+_id,
+projectId,
+user,
+from,
+to: PROJECT_STATUSES.COMPLETED, 
+comment: null,
+completedAt: new Date() 
+  };
 };
 
-export { createProjectLog, moveToOnSiteInspectionScheduled };
+
+// closing out status below
+const ProjectStatusLogClosing = (currentUser, project, from) => {
+  if(currentUser.role !== USER_ROLES.ADMIN && currentUser.role !== USER_ROLES.SALES_REP){
+    throw 'Only the admins and sales reps can update project status to closing out';
+  }
+  const _id = new ObjectId();
+  const projectId = new ObjectId(project._id);
+  const user = {
+    _id: new ObjectId(currentUser._id),
+    email: currentUser.email,
+    role: currentUser.role,
+  };
+  return {
+_id,
+projectId,
+user,
+from,
+to: PROJECT_STATUSES.CLOSING_OUT, 
+comment: null
+};
+};
+
+
+// changing the status log to installationcomplete
+const projectStatusLogChange = (currentUser, project, from) => {
+  if (currentUser.role !== USER_ROLES.ADMIN && currentUser.role !== USER_ROLES.SALES_REP){
+    throw 'Only admins and sales reps can update project status to validating permits';
+  }
+
+const to = PROJECT_STATUSES.VALIDATING_PERMITS;
+const installationCompletedAt = new Date();
+const _id = new ObjectId();
+const projectId = new Object(project._id);
+const user = {
+  _id: new ObjectId(currentUser._id),
+  email: currentUser.email,
+  role: currentUser.role,
+};
+  return {
+  _id,
+  projectId,
+  user,
+  from,
+  to,
+  comment: null,
+  installationCompletedAt
+  };
+};
+
+
+export { createProjectLog, moveToOnSiteInspectionScheduled, ProjectStatusLog, ProjectStatusLogClosing, projectStatusLogChange };
--- hashroot-be/routes/projectStatuses.js
+++ hashroot-be/routes/projectStatuses.js
@@ -1,9 +1,6 @@
 import { Router } from "express";
 import { PROJECT_STATUSES, PROJECT_STATUS_KEYS } from "../constants.js";
-import {
-  createProjectLog,
-  moveToOnSiteInspectionScheduled,
-} from "../data/projectStatuses.js";
+import {createProjectLog, moveToOnSiteInspectionScheduled,ProjectStatusLog, ProjectStatusLogClosing, projectStatusLogChange} from "../data/projectStatuses.js";
 
 const router = Router();
 
@@ -27,6 +24,61 @@ router
       res.json({ project });
     } catch {
       return res.status(404).json({ error: error?.toString() });
+    }
+  });
+
+  router
+  .route(`/${PROJECT_STATUS_KEYS.COMPLETED}`)
+  .patch(async (req, res) => {
+    try {
+      const project = req.project;
+      project.status = PROJECT_STATUSES.COMPLETED;
+      project.completedAt = new Date();
+      await req.project.save();
+      await createProjectLog(
+        req.user,
+        req.project,
+        req.project.status,
+        PROJECT_STATUSES.COMPLETED,
+        `Project marked as completed on ${project.completedAt}`
+      );
+
+      res.json({ project });
+    } catch (error) {
+      return res.status(404).json({ error: error?.toString() });
+    }
+  });
+
+  router
+  .route(`/${PROJECT_STATUS_KEYS.CLOSING_OUT}`)
+  .patch(async (req, res) => {
+    try {
+      const project = req.project;
+      const result = await createProjectLog(
+        req.user,
+        project,
+        result,
+        req.project.status,
+        PROJECT_STATUSES.CLOSING_OUT,
+        `Project marked as closing out`
+      );
+      res.json({ success: true });
+    }catch(error){
+        return res.status(404).json({ error: error?.toString() });
+}
+  });
+
+  router.patch(`/${PROJECT_STATUS_KEYS.VALIDATING_PERMITS}`, async (req, res) => {
+    try {
+      const project = req.project;
+      const statusLogChange = projectStatusLogChange(req.user, project, project.status);
+      project.status = PROJECT_STATUSES.VALIDATING_PERMITS;
+      project.installationCompletedAt = statusLogChange.installationCompletedAt;
+      await req.project.save();
+      await createProjectLog(req.user,req.project,statusLogChange.from,PROJECT_STATUSES.VALIDATING_PERMITS,`Project status changed to VALIDATING_PERMITS`)
+      res.json({ project });
+    } catch(error){
+        return res.status(404).json({ error: error?.toString() });
     }
   });
 
